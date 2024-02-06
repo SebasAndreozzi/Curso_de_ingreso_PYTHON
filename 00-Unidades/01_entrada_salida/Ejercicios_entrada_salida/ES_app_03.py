98b98842b09f236c38166f7ba9a67d35cb80c032
SebasAndreozzi <sebastian.javier.andreozzi@gmail.com>
2024-02-06 11:51:51 -0300

    Entrega

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_03.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_03.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Sebasatián Javier
+apellido: Andreozzi
 ---
 Ejercicio: entrada_salida_03
 ---
@@ -34,7 +34,10 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        title = "Nombre"
+        nom_ingresado = self.txt_nombre.get()
+
+        alert(title, nom_ingresado)
                 
     
 if __name__ == "__main__":
