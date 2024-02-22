58eece6615532f1b31352a76b0b58cd7c200881a
SebasAndreozzi <sebastian.javier.andreozzi@gmail.com>
2024-02-22 11:51:02 -0300

    Entrega_!

--- 00-Unidades/04_while/TP_While/05_While_Validaciones_rising_btl.py
+++ 00-Unidades/04_while/TP_While/05_While_Validaciones_rising_btl.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Sebastián Javier
+apellido: Andreozzi
 ---
 TP: While_validaciones_rising_btl
 ---
@@ -55,7 +55,88 @@ class App(customtkinter.CTk):
         self.btn_validar.grid(row=4, pady=20, columnspan=2, sticky="nsew")
 
     def btn_validar_on_click(self):
-        pass
+        MIN_AGE = 18
+        MAX_AGE = 90
+
+        SINGLE = "S"
+        SINGLE_TEXT = "Soltero/a"
+        MARRIED = "C"
+        MARRIED_TEXT = "Casado/a"
+        DIVORSED = "D"
+        DIVORSED_TEXT = "Divorciado/a" 
+        WIDOW = "V"
+        WIDOW_TEXT = "Viudo/a"
+
+        CHARACTERS_FILE_NUMBER = 4
+
+        title_prompt = "Completar" 
+
+        #SURNAME
+        message_surname = "Apellido: "
+
+        surname_entry = prompt(title_prompt, message_surname)
+
+        while surname_entry == None or surname_entry == "":
+            surname_entry = prompt(title_prompt, message_surname)
+        
+        self.txt_apellido.delete(0, tkinter.END)
+        self.txt_apellido.insert(0, surname_entry)
+        
+        #AGE
+        message_age = "Edad (entre 18 y 90 años): "
+
+        age_entry = prompt(title_prompt, message_age)
+
+        while age_entry == None or age_entry == "" or MIN_AGE > int(age_entry) or int(age_entry) > MAX_AGE:
+            age_entry = prompt(title_prompt, message_age)
+        
+        self.txt_edad.delete(0, tkinter.END)
+        self.txt_edad.insert(0, age_entry)
+
+        #MARITAL STATE
+        message_marital_state = "Complete su Estado Civil con alguna de estas letras: \n   S = Soltero/a \n   C = Casado/a \n   D = Divorciado/a \n   V = Viudo/a"
+
+        marital_state_entry = prompt(title_prompt, message_marital_state)
+        
+        while marital_state_entry != SINGLE and marital_state_entry != MARRIED and marital_state_entry != DIVORSED and marital_state_entry != WIDOW:
+            marital_state_entry = marital_state_entry = prompt(title_prompt, message_marital_state)
+        
+        match marital_state_entry:
+            case "S":
+                self.combobox_tipo.set(SINGLE_TEXT)
+            case "C":
+                self.combobox_tipo.set(MARRIED_TEXT)
+            case "D":
+                self.combobox_tipo.set(DIVORSED_TEXT)
+            case _:
+                self.combobox_tipo.set(WIDOW_TEXT)
+                    
+        '''
+
+        match marital_state_entry:
+            case "S":
+                self.combobox_tipo.set(SINGLE_TEXT)
+            case "C":
+                self.combobox_tipo.set(MARRIED_TEXT)
+            case "D":
+                self.combobox_tipo.set(DIVORSED_TEXT)
+            case "V":
+                self.combobox_tipo.set(WIDOW_TEXT)
+            case _:
+                marital_state_entry = prompt(title_prompt, message_marital_state)
+
+        '''
+
+        #FILE NUMBER
+        message_file_num = "Nro de legajo: "
+
+        file_num_entry = prompt(title_prompt, message_file_num)
+
+        while file_num_entry == None or file_num_entry == "" or len(file_num_entry) != CHARACTERS_FILE_NUMBER:
+            file_num_entry = prompt(title_prompt, message_file_num)
+        
+        self.txt_legajo.delete(0, tkinter.END)
+        self.txt_legajo.insert(0, file_num_entry)            
 
 
 if __name__ == "__main__":
--- 00-Unidades/04_while/TP_While/06_While_Elecciones_Paso.py
+++ 00-Unidades/04_while/TP_While/06_While_Elecciones_Paso.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Sebastián Javier
+apellido: Andreozzi
 ---
 TP: While_elecciones_paso
 ---
@@ -35,7 +35,88 @@ class App(customtkinter.CTk):
         self.btn_validar.grid(row=4, pady=20, columnspan=2, sticky="nsew")
 
     def btn_validar_on_click(self):
-        pass
+        counter_candidates = 0
+        counter_votes = 0
+
+        most_votes = 0
+        most_voted_candidate = None
+
+        least_votes = 0
+        least_voted_candiate = None
+        least_voted_candidate_age = 0
+
+        age_sum = 0
+        average_age = 0
+
+        title_prompt = "Completar"
+
+        while True:
+            candidate_name = prompt(title_prompt, "Nombre:")
+
+            if candidate_name == None:
+                break
+            else:
+                #CANDIDATE NAME
+
+                while candidate_name == "":
+                    candidate_name = prompt(title_prompt, "Nombre:")
+                
+                #CANDIDATE AGE
+                
+                candidate_age = prompt(title_prompt, "Edad: ")
+
+                while candidate_age == None or candidate_age == "":
+                    candidate_age = prompt(title_prompt, "Edad: ")
+                
+                candidate_age = int(candidate_age)
+
+                #VOTES CUANTITY
+                    
+                votes_cuantity = prompt(title_prompt, "Cantidad de votos: ")
+
+                while votes_cuantity == None or votes_cuantity == "":
+                    votes_cuantity = prompt(title_prompt, "Cantidad de votos: ")
+                
+                votes_cuantity = int(votes_cuantity)
+
+                #MOST AND LEAST VOTED CANDIDATE
+
+                if most_votes == 0 and least_votes == 0:
+                    most_votes = votes_cuantity
+                    most_voted_candidate = candidate_name.capitalize()
+                    least_votes = votes_cuantity
+                    least_voted_candiate = candidate_name.capitalize()
+                    least_voted_candidate_age = candidate_age
+
+                #MOST VOTED CANDIDATE
+
+                if votes_cuantity > most_votes:
+                    most_votes = votes_cuantity
+                    most_voted_candidate = candidate_name.capitalize()
+                else:
+
+                    #LEAST VOTED CANDIDATE NAME & AGE
+
+                    if votes_cuantity < least_votes:
+                        least_votes = votes_cuantity
+                        least_voted_candiate = candidate_name.capitalize()
+                        least_voted_candidate_age = candidate_age
+                
+                #AGE SUM
+                        
+                age_sum = age_sum + candidate_age
+
+                #COUNTERS: VOTES & CANDIDATES
+
+                counter_votes = counter_votes + votes_cuantity
+                counter_candidates += 1
+
+        average_age = age_sum / counter_candidates
+
+        title = "Resultado"
+        message = f"El candidato con más votos es: {most_voted_candidate}.\nEl candidato con menos votos es: {least_voted_candiate} de {least_voted_candidate_age} años.\nEl promedio de edades de los candidatos es: {average_age} años.\nEl total de votos es de {counter_votes}."
+        
+        alert(title, message)
 
 
 if __name__ == "__main__":
